be7f9a3649ade7fc551974547442b4c42eacb9d6
Bao Nguyen <Bao Nguyen@DESKTOP-OCHAU49>
2019-12-11 00:26:51 -0500

    Version 1

---
+++ ClinicV2/Controllers/AdminOptionsController.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using System.Web.Mvc;
+using clinic.Models;
+using System.Activities;
+
+namespace ClinicV2.Controllers
+{
+    public class AdminOptionsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddClinics()
+        {
+            ViewBag.Message = "Add a New Clinic page.";
+            return View();
+        }
+
+        public ActionResult EditClinics()
+        {
+            ViewBag.Message = "Edit Clinics page.";
+            return View();
+        }
+
+        public ActionResult DeleteClinics()
+        {
+            ViewBag.Message = "Delete Clinics page.";
+            return View();
+        }
+
+        public ActionResult ViewDemographics()
+        {
+            ViewBag.Message = "View Demographics page.";
+            return View();
+        }
+
+
+        public ActionResult ClinicAdded()
+        {
+            ViewBag.Message = "Clinic added";
+            return View();
+        }
+
+         public ActionResult FindClinics()
+            {
+                ViewBag.Message = "Find Clinics page.";
+
+                return View();
+            }
+
+            
+            public ActionResult AdminOptions()
+            {
+                ViewBag.Message = "Admin Options page.";
+                return View();
+            }
+
+            public ActionResult AdminLogin()
+            {
+               
+
+            //Connection to database (Login credentials retrieval at a later date)
+             
+            return View(); 
+
+            }
+
+        public ActionResult Logout()
+        {
+            ViewBag.Message = "Logout";
+            return RedirectToAction("Index","Info");
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //    public IActionResult Error()
+        //    {
+        //        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //    }
+    
+        //[responsecache(duration = 0, location = responsecachelocation.none, nostore = true)]
+        //public iactionresult error()
+        //{
+        //    return view(new errorviewmodel { requestid = activity.current?.id ?? httpcontext.traceidentifier });
+        //}
+    }
+}
---
+++ ClinicV2/Models/Credentials.cs
@@ -0,0 +1,75 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicV2.Models
+{
+    public class Credentials
+    {
+        public string ID { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public List<Credentials> Cred { get; set; }
+
+
+        public string CompareCredentials(string Username, string Pass)
+        {
+            List<Credentials> validCredentials = new List<Credentials>();
+            MySqlConnection cnn;
+
+            string indicator = "";
+            string connString = @"Server=clinicsystemdb.cfkpw0ap0abf.us-east-1.rds.amazonaws.com;user id=Lotusep5ep; Pwd=Pat123forsell; database=ClinicSysDB";
+
+            cnn = new MySqlConnection(connString);
+
+            MySqlDataReader reader = null;
+
+            cnn.Open();
+            string sql = "Select * from Users";
+
+            MySqlCommand command = new MySqlCommand(sql, cnn);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                validCredentials.Add(new Credentials
+                {
+                    ID = reader.GetValue(0).ToString(),
+
+                    UserName = reader.GetValue(1).ToString(),
+
+                    Password = reader.GetValue(2).ToString(),
+
+                    Cred = new List<Credentials>()
+
+                });
+            }
+
+            foreach (Credentials CMD in validCredentials)
+            {
+                if (CMD.UserName != Username && CMD.Password != Pass)
+                {
+                    indicator = "Login failed"; // Login Failed
+                }
+
+                else if (CMD.UserName == Username && CMD.Password != Pass)
+                {
+                    indicator = "The password you entered is incorrect, please try again."; // Password is incorrect
+                }
+                else if (CMD.UserName != Username && CMD.Password == Pass)
+                {
+                    indicator = "Invalid username"; //Username is incorrect 
+                }
+                else
+                {
+                    indicator = "Login Successful"; // Login Successful 
+                    return indicator;
+                }
+            }
+
+            return indicator;
+
+        }
+    }
+}
